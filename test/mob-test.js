9138573a635c3644500d7c60191633fc32be7f0e
= <=>
2019-08-10 18:22:40 -0700

    adds basic mocha-chai-expect starter

---
+++ test/mob-test.js
@@ -0,0 +1,15 @@
+var expect    = require("chai").expect;
+var mob = require("../app/mob");
+
+describe("The mob", function(){
+
+    describe("mobTest", function(){
+        it("makes this test pass", function() {
+            var actual = mob.mobTest();
+            expect(actual).to.be.true;
+
+        });
+    });
+
+});
+
